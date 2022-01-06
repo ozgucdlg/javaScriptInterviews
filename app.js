dd6770f76e669ebbdf2a7070308d5b8668e1d158
DALGA Ozguc <ozgucdalga@gmail.com>
2022-01-05 23:51:42 -0500

    how to find max character on string

--- app.js
+++ app.js
@@ -9,3 +9,13 @@ const revString = (str)=>{
 revString("elma");
 
 
+// how to find max character in a string and how many times it appears in the
+
+const maxChar = (str)=>{
+    let obj = {};
+    for (let char of str)
+    (!obj[char])? obj[char] =1 : obj[char]++;
+    console.log(obj);
+
+}
+maxChar("elma");
