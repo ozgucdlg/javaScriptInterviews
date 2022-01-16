c60945a85d6c48467733da03f5b4773191acddd8
DALGA Ozguc <ozgucdalga@gmail.com>
2022-01-15 23:45:19 -0500

    switch case

--- app.js
+++ app.js
@@ -23,3 +23,50 @@ maxChar("elma");
 
 
 //  how to polybdrom
+
+
+
+
+const list = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
+
+const askForPeople = propmt("what is yout Fvaorite day");
+
+switch (List) {
+    case 1:
+        askForPeople === "Monday"
+
+        alert(`your decision is Monday are you sure !! `)
+        break;
+
+    case 1:
+        askForPeople === "Tuesday"
+
+        alert(`your decision is Tuesday are you sure !! `)
+        break;
+
+    case 1:
+        askForPeople === "Wednesday"
+
+        alert(`your decision is Wednesday are you sure !! `)
+        break;
+    case 1:
+        askForPeople === "Thursday"
+
+        alert(`your decision is Thursday are you sure !! `)
+        break;
+    case 1:
+        askForPeople === "Friday"
+
+        alert(`your decision is Friday are you sure !! `)
+        break;
+    case 1:
+        askForPeople === "Saturday"
+
+        alert(`your decision is Saturday are you sure !! `)
+        break;
+    case 1:
+        askForPeople === "Sunday"
+
+        alert(`your decision is Sunday are you sure !! `)
+        break;
+}
